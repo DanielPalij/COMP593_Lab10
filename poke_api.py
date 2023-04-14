195e7ce639c822353a69040fed504cb429342a3a
DanielPalij <daniel.palij@flemingcollege.ca>
2023-04-13 20:44:48 -0400

    Completed api

--- poke_api.py
+++ poke_api.py
@@ -1,13 +1,15 @@
 import requests
+import image_lib
+import os
 
 poke_api = 'https://pokeapi.co/api/v2/pokemon/'
 
 def main():
-    #pinfo = get_pokemon_info("Pikachu")
-   # pinfo = get_pokemon_info(123)
-   # names = get_pokemon_names()
+    pinfo = get_pokemon_info("Pikachu")
+    pinfo = get_pokemon_info(123)
+    names = get_pokemon_names()
     
-    download_pokemon_artwork('ditto', '')
+    download_pokemon_artwork('ditto', r'C:\Users\Danie\OneDrive\Desktop\Random')
     
     return
 
@@ -32,41 +34,44 @@ def get_pokemon_names(offset=0, limit=100000):
     query_str_params = {
         'offset' : offset,
         'limit' : limit,
-
-
     }
+    
     print(f'Getting list of Pokemon names...', end='')
     resp_msg = requests.get(poke_api, params=query_str_params)
 
     if resp_msg.status_code == requests.codes.ok:
-        print('success')
+        print('Success')
         pokemon_dict = resp_msg.json()
         pokemon_names_list = [p['name']for p in pokemon_dict['results']]
         return pokemon_names_list
     else:
-        print('failure')
+        print('Failure')
         print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
         return
     
 
-def download_pokemon_artwork(pokemon_name):
+def download_pokemon_artwork(pokemon_name, save_dir):
     
+    # Get all info for the specified pokemon
     pokemon_info = get_pokemon_info(pokemon_name)
     if pokemon_info is None:
         return
 
+    # Extract the artwork URL from the info dictionary
     artwork_url = pokemon_info['sprites']['other']['official-artwork']['front_default']
 
+    
+    image_bytes = image_lib.download_image(artwork_url)
+    if image_bytes is None: 
+        return
+    
 
-
-    return
-
-
-
-
-
-
-
+    file_ext = artwork_url.split('.')[-1]
+    image_path = os.path.join(save_dir, f'{pokemon_name}.{file_ext}')
+    
+    # Saves the image
+    if image_lib.save_image_file(image_bytes, image_path):
+        return image_path
 
 if __name__ == '__main__':
         main()
